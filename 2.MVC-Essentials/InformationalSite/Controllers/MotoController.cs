725450a7dc41718723019e26325f14485b112f98
asd <gparlakov@abv.bg>
2013-09-25 12:07:26 -0700

    2.MVC Essentials Created informational website

---
+++ 2.MVC-Essentials/InformationalSite/Controllers/MotoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InformationalSite.Controllers
+{
+    public class MotoController : Controller
+    {
+        //
+        // GET: /Moto/enduro
+        [HttpGet]
+        [ActionName("enduro")]
+        public ActionResult GetEnduro()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Moto/sport
+        [HttpGet]
+        [ActionName("sport")]
+        public ActionResult GetSport()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Moto/tourer
+        [HttpGet]
+        [ActionName("tourer")]
+        public ActionResult GetTourer()
+        {
+            return View();
+        }
+	}
+}
