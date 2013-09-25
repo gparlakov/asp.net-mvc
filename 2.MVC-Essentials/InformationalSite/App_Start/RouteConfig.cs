d803f6da1b5221ac8508065b09cccd60e0b724d9
gparlakov <gparlakov@abv.bg>
2013-09-25 13:22:09 -0700

    2.MVC essentials - added secret route and a partial view for the forums and stores

--- 2.MVC-Essentials/InformationalSite/App_Start/RouteConfig.cs
+++ 2.MVC-Essentials/InformationalSite/App_Start/RouteConfig.cs
@@ -13,10 +13,32 @@ namespace InformationalSite
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //
+            // MotoSpecialRoute/Moto/123 - 3 digits
+            routes.MapRoute(
+                name: "AdminSpecialRoute",
+                url: "{special}/{number}",
+                defaults: new
+                {
+                    controller = "Secret",
+                    action = "Number"
+                },
+                constraints: new 
+                {
+                    special= "MotoSpecialRoute",
+                    number = @"[\w]{3}"
+                }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new 
+                { 
+                    controller = "Home", 
+                    action = "Index", 
+                    id = UrlParameter.Optional 
+                }
             );
         }
     }
---
+++ 2.MVC-Essentials/InformationalSite/Areas/Admin/Controllers/SecretController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InformationalSite.Areas.Admin.Controllers
+{
+    public class SecretController : Controller
+    {
+        //
+        // GET: /MotoSpecialRoute/123   // 3 digits
+        [ActionName("number")]
+        public ActionResult SecretNumber(string number)
+        {
+            ViewBag.number = number;
+            return View("~/Areas/Admin/Views/Secret/Number.cshtml");
+        }
+	}
+}
