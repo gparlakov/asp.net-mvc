770b6d4c33a89f77da12603fadb0a9e2b661c4ce
gparlakov <gparlakov@abv.bg>
2013-10-05 15:42:07 -0700

    Add jquery unobtrusive

---
+++ Twitter-Like/Teleritter/Models/NewTelereetViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Teleritter.Models
+{
+    public class NewTelereetViewModel
+    {
+        [Required, StringLength(1000, ErrorMessage="Max length is 1000 symbols! Min is 2.", MinimumLength = 2)]
+        public string Text { get; set; }
+        
+        public string Tags { get; set; }
+          
+        public string NewTag { get; set; }
+    }
+}
